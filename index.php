62908c1440fdb1aa2861700ba699c881fcedaf8c
anveshpandey <40883628+anveshpandey@users.noreply.github.com>
2023-03-23 01:24:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php require_once 'config.php' ;?>
+<?php session_start();?>
+<?php require_once 'config.php';?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +13,14 @@
 	<input type="checkbox" id="chk" aria-hidden="true">
     <?php if(isset($_GET['error'])) { ?>
             <p class="error"> <?php  $_GET["error"]; ?></p>
-        <?php } ?>
+        <?php } ?> 
+        <?php $token = md5(uniqid(rand(),true)); ?>
+        <?php $_SESSION['csrf_token'] = $token; ?>
+
         
     <form action="login.php" method="post">
     <label for="chk" aria-hidden="true">Sign In</label>
-        <input type="hidden" name= "csrf_token" value="<?php echo createToken(); ?>">
+        <input type="hidden" name="csrf_token" value="<?php echo $token;?>">
         <input type="text" name="email" placeholder="Email ID" required=""><br>
         <input type="password" name="password" placeholder="Password" required=""><br>
         <button type="submit">Login</button><br>
